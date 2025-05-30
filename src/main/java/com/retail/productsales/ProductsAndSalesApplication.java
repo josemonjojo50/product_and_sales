79fce661030e45dc248e522b7a0b8b9b835eb251
josemonjojo50 <josemonjojo50@gmail.com>
2025-05-30 11:39:17 +0530

    Main method

--- src/main/java/com/retail/productsales/ProductsAndSalesApplication.java
+++ src/main/java/com/retail/productsales/ProductsAndSalesApplication.java
@@ -15,37 +15,30 @@ import java.time.LocalDate;
 @SpringBootApplication
 public class ProductsAndSalesApplication implements CommandLineRunner {
 
-    @Autowired
-    private ProductService productService;
+	@Autowired
+	private ProductService productService;
 
-    @Autowired
-    private SaleService saleService;
+	@Autowired
+	private SaleService saleService;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProductsAndSalesApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsAndSalesApplication.class, args);
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        
-        Product product1 = new Product("Product 1", "Description 1", 100.0, 10);
-        Product product2 = new Product("Product 2", "Description 2", 200.0, 20);
+	@Override
+	public void run(String... args) throws Exception {
 
-        productService.addProduct(product1);
-        productService.addProduct(product2);
+		Product product1 = new Product("Product 1", "Description 1", 100.0, 10);
+		Product product2 = new Product("Product 2", "Description 2", 200.0, 20);
 
-        Sale sale1 = new Sale(0, product1, 2, LocalDate.now());
-        Sale sale2 = new Sale(0, product2, 3, LocalDate.now());
+		productService.addProduct(product1);
+		productService.addProduct(product2);
 
-        saleService.addSale(sale1);
-        saleService.addSale(sale2);
+		Sale sale1 = new Sale(0, product1, 2, LocalDate.now());
+		Sale sale2 = new Sale(0, product2, 3, LocalDate.now());
 
-        
-        double totalRevenue = productService.getTotalRevenue();
-        double revenueByProduct1 = productService.getRevenueByProduct(product1.getId());
+		saleService.addSale(sale1);
+		saleService.addSale(sale2);
 
-        System.out.println("Total Revenue: " + totalRevenue);
-        System.out.println("Revenue for Product 1: " + revenueByProduct1);
-    }
+	}
 }
-
